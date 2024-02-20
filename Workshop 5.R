d36325804619def3ec67a563e6159a811173bbc0
Dalia-ES <d.el-shoura@se20.qmul.ac.uk>
2024-02-20 13:18:43 +0000

    Use read.table, and pivot functions

--- Workshop 5.R
+++ Workshop 5.R
@@ -26,3 +26,34 @@ usites <- unique(beetles4$Site)
 length(usites)
 colnames(beetles4)[3:ncol(beetles4)]
 
+str(beetles4) 
+summary(beetles4)
+head(beetles4)
+View(beetles4)
+
+beetlesdf <- read.table("dung_beetles_read_1.csv", sep=",",header=T)
+beetlesdf
+
+beetles2df <- read.table("dung_beetles_v2.csv", sep=",",header=T)
+
+beetles3df <- read.table("dung_beetles_v3.csv", sep=",",header=T)
+
+library(tidyr)
+?fill
+
+fill(beetlesdf,Site)
+beetlesdf <- fill(beetlesdf,Site)
+
+beetlesdf <- read.table("dung_beetles_read_1.csv", sep=",",header=T) %>% fill(Site)
+
+?pivot_longer
+pivot_longer(data=beetlesdf, cols = c("Caccobius_bawangensis", "Catharsius_dayacus", "Catharsius_renaudpauliani", "Copis_agnus", "Copis_ramosiceps", "Copis_sinicus", "Microcopis_doriae", "Microcopis_hidakai"),names_to="Spp")
+
+pivot_longer(data=beetlesdf, cols = starts_with("C") )
+
+casesdf <- read.table("WMR2022_reported_cases_1.txt",sep="\t")
+casesdf
+
+pivot_wider(casesdf,names_from="method",values_from ="n")
+
+sessionInfo()
